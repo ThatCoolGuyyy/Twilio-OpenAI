613ad179520e630ce7005e72fc41ee01af71cdbb
ThatCoolGuyyy <goldendarmie@gmail.com>
2024-01-24 16:52:34 +0100

    Add voice and chat routes to API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VoiceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/voice', [VoiceController::class, 'voiceOutput']);
+Route::post('/chat', [VoiceController::class, 'speechtoText']);
+Route::post('/endCall', [VoiceController::class, 'endCall']);
+Route::post('/makeCall', [VoiceController::class, 'makeCall']);
